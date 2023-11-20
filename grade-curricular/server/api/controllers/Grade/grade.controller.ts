af66b0073845d541cf49a2b0683dcd9a8622752a
rafauniville <104468327+rafauniville@users.noreply.github.com>
2023-11-20 02:38:42 +0000

    test get

--- grade-curricular/server/api/controllers/Grade/grade.controller.ts
+++ grade-curricular/server/api/controllers/Grade/grade.controller.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from 'express';
-import gradeService from 'server/api/services/grade.service';
+import gradeService from '../../services/grade.service';
 
 class GradeController {
   all(_: Request, res: Response): void {
     gradeService.all().then((r) => res.json(r));
+  }
+  getById(req: Request, res: Response): void {
+    if (req.params['id'] == undefined || req.params['id'] == '')
+      res.status(400).end();
+    gradeService.getById(req.params['id']).then((r) => res.json(r));
+  }
+  post(req:Request, res:Response): void{
+    if(req.body == undefined)
+        res.status(400).end();
+    gradeService.saveNew(req.body).then((r) => res.json(r));
   }
 }
 
--- grade-curricular/server/api/services/grade.service.ts
+++ grade-curricular/server/api/services/grade.service.ts
@@ -1,5 +1,5 @@
-import { Container } from '@azure/cosmos';
-import cosmosdb from 'server/common/cosmosdb';
+import { Container, SqlQuerySpec } from '@azure/cosmos';
+import cosmosdb from '../../../server/common/cosmosdb';
 import { Grade } from '../entites/Grade';
 
 class GradeService {
@@ -12,6 +12,24 @@ class GradeService {
 
     return Promise.resolve(listaGrade);
   }
+  async getById(id: string): Promise<Grade> {
+    const querySpec: SqlQuerySpec = {
+      query: 'SELECT * FROM Grade g WHERE g.id = @id',
+      parameters: [{ name: '@id', value: id }],
+    };
+    const { resources: listaGrade } = await this.container.items
+      .query(querySpec)
+      .fetchAll();
+
+    return Promise.resolve(listaGrade[0]);
+  }
+  async saveNew(grade:Grade): Promise<Grade>{
+    grade.id = "";
+    await this.container.items.create(grade);
+    
+    return Promise.resolve(grade);
+  }
+
 }
 
 export default new GradeService();
